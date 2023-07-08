87ef6e9951036e52dc8970383b36fd061cd7a6c3
Yazan Habash <54286064+Yazan-Habash114@users.noreply.github.com>
2023-07-08 12:09:34 +0300

    Update design.sv

--- design.sv
+++ design.sv
@@ -2,40 +2,54 @@
 `include "Receiver.sv"
 `include "Transmitter.sv"
 
-module UART #(parameter DATA_BITS = 8, SB_TICKS = 1, IS_PARITY = 0, PARITY = 0) (
+module UART (
   input wire clk,
   input wire reset,
-  input wire [31:0] ticks_per_bit,
   input wire rx,
   input wire tx_start,
-  input wire [DATA_BITS - 1:0] tx_data_in,
+  input wire parity_type,
+  input wire parity_en,
+  input wire stop2,
+  input wire [1:0] baud,
+  input wire [3:0] frame_length,
+  input wire [7:0] tx_data_in,
   output reg correct,
   output reg rx_done,
   output reg tx_done,
   output reg tx,
-  output reg [DATA_BITS - 1:0] rx_data_out
+  output reg [7:0] rx_data_out
 );
+  
   wire tick;
   
   BaudRateGenerator brg(
     .clk(clk),
-    .ticks_per_bit(ticks_per_bit),
+    .reset(reset),
+    .baud(baud),
     .tick(tick)
   );
   
-  Transmitter #(DATA_BITS, SB_TICKS, IS_PARITY, PARITY) transmitter(
+  Transmitter transmitter(
     .tx_tick(tick),
     .tx_start(tx_start),
     .reset(reset),
+    .frame_length(frame_length),
     .tx_din(tx_data_in),
+    .parity_type(parity_type),
+    .parity_en(parity_en),
+    .stop2(stop2),
     .tx(tx),
     .tx_done(tx_done)
   );
   
-  Receiver #(DATA_BITS, SB_TICKS, IS_PARITY, PARITY) receiver(
+  Receiver receiver(
     .rx(rx),
     .rx_tick(tick),
     .reset(reset),
+    .frame_length(frame_length),
+    .parity_type(parity_type),
+    .parity_en(parity_en),
+    .stop2(stop2),
     .rx_dout(rx_data_out),
     .rx_done(rx_done),
     .correct(correct)
